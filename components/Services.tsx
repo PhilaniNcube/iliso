cf1ef2598eec1394bb61e8ae38eb1e533b07f1af
Philani Ncube <ncbphi001@gmail.com>
2022-04-14 11:47:34 +0200

    contact

--- components/Services.tsx
+++ components/Services.tsx
@@ -19,7 +19,81 @@ const Services = () => {
 <p className="text-6xl font-bold" >&#x2B;</p>
 <p className="text-base font-bold" >Development Rights Acquisition</p>
 </div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Property Identification, audit and selection</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Rezoning of Land</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Permanent Departure</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Subdivision of land</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Consolidation of properties</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Removal of restrictive title deed conditions</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Consent Use Applications</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Acquisition of liquor Licenses</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Application for Cellphone Towers</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Business Advertising Boards</p>
+</div>
+<div className="flex flex-col rounded shadow-lg hover: justify-center items-center p-6 bg-green-700 hover:bg-sky-700 text-base font-bold transition-colors duration-150 text-center space-y-4 text-white">
+<p className="text-6xl font-bold" >&#x2B;</p>
+<p className="text-base font-bold" >Property Management</p>
+</div>
 
+                </div>
+
+                 <div className="">
+
+            
+            <div className="max-w-6xl mx-auto py-24 ">
+                <h1 className="text-sky-700 uppercase font-mont-extrabold text-2xl md:text-3xl lg:text-5xl">PARTNERSHIPS & <span className="text-green-700 font-mont-medium">AFFILIATE ASSOCIATIONS</span></h1>
+                <p className="text-sm text-gray-700 max-w-[90ch] py-3">We take pride in our association with the following partners and affiliates:</p>
+                    </div>
+                    <p className="text-xl uppercase">Clients:</p>
+                    <div className="max-w-6xl mx-auto py-6 grid grid-cols-2 md:grid-cols-3 gap-4">
+                    <img src="/images/casso.jpeg" alt="logo" className="h-[100px] object-contain"  />
+                    <img src="/images/phane.jpeg" alt="logo" className="h-[100px] object-contain" />
+                    <img src="/images/mprojects.png" alt="logo" className="h-[100px] object-contain" />
+                   
+                    </div> 
+                    <p className="text-xl uppercase mt-20">Partnerships:</p>
+                    <div className="max-w-6xl mx-auto py-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <img src="/images/vena.jpeg" alt="logo" className="h-[100px] object-contain"  />
+                    <img src="/images/zweb.png" alt="logo" className="h-[100px] object-contain" />
+                    <img src="/images/ilizwe.jpeg" alt="logo" className="h-[100px] object-contain" />
+                    <img src="/images/mmp.png" alt="logo" className="h-[100px] object-contain" />
+                    <img src="/images/mmp.png" alt="logo" className="h-[100px] object-contain" />
+                    </div> 
+                    <p className="text-xl uppercase mt-20">Affiliates:</p>
+                    <div className="max-w-6xl mx-auto py-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    <img src="/images/sac.png" alt="logo" className="h-[100px] object-contain"  />
+                    <img src="/images/saacpp.jpeg" alt="logo" className="h-[100px] object-contain" />
+                     </div> 
+                
                 </div>
 
             </div>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -11,7 +11,48 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>Contact</h1>
+      <div className=" grid grid-cols-1 md:grid-cols-2">
+        <img src="/images/contact.jpeg" alt='contact' className="w-full object-cover" />
+        <div className="bg-sky-700 text-white p-6">
+<h1 className="text-2xl font-mont-bold">Contact Us</h1>
+          <form className="mt-8 w-4/6">
+            <div className="flex flex-col">
+              <label htmlFor="name" className="text-lg">Name</label>
+            <input type="text" className="border-b-2 border-white bg-transparent"  name="name" id="name"/>
+            </div>
+            <div className="flex flex-col mt-8">
+              <label htmlFor="contactNumber" className="text-lg">Contact Number</label>
+            <input type="tel" className="border-b-2 border-white bg-transparent"  name="contactNumber" id="contactNumber"/>
+            </div>
+            <div className="flex flex-col mt-8">
+              <label htmlFor="email" className="text-lg">Email Address</label>
+            <input type="email" className="border-b-2 border-white bg-transparent"  name="email" id="email"/>
+            </div>
+            <div className="flex flex-col mt-8">
+              <label htmlFor="message" className="text-lg">Message</label>
+            <input type="message" className="border-b-2 border-white bg-transparent"  name="message" id="message"/>
+            </div>
+</form>
+        </div>
+        <div className="h-[500px] bg-green-700 text-white px-6 lg:px-16 py-12 flex flex-col justify-center">
+          <div className="flex space-x-8 justify-between">
+            
+            <p className="font-mont-bold mb-6">Address</p> <p>33 Maninjwa Street, Northcrest, Mthatha, 5099</p>
+         
+          </div>
+          
+          <div className="flex space-x-8 justify-between">   <p className="font-mont-bold mb-6">Cell</p> <p>+27 72 742 0073
+(Also available on Whatsapp)</p>
+          </div>
+          
+          <div className="flex space-x-8 justify-between"> <p className="font-mont-bold mb-6">Cell</p> <p className="flex flex-col"><span>ilisoplanners01@gmail.com</span>
+          <span>info@ilisourbanplanners.co.za</span>
+          </p></div>
+
+        </div>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3399.5451025399425!2d28.778027514474598!3d-31.564096581355145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e5fda1026181cc1%3A0x1c22ce5078e788d9!2s33%20V.%20N.%20Maninjwa%20St%2C%20Northcrest%2C%20Mthatha%2C%205099!5e0!3m2!1sen!2sza!4v1649929473033!5m2!1sen!2sza" width="100%" height="100%"   loading="lazy" ></iframe>
+  </div>
 
      
     </Fragment>
